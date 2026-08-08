862ef940a499c59e6fdf92d182beaff085e04f41
Art Kart <akartushin@alfabank.ru>
2026-08-08 15:54:04 +0300

    List chapter has been supplemented

--- src/main/kotlin/pro/artkart/kmagic/list/ImmutableList.kt
+++ src/main/kotlin/pro/artkart/kmagic/list/ImmutableList.kt
@@ -10,8 +10,8 @@ sealed class ImmutableList<T> {
     }
 
     private class Cons<T>(
-        internal val head: T,
-        internal val tail: ImmutableList<T>
+        val head: T,
+        val tail: ImmutableList<T>
     ) : ImmutableList<T>() {
 
         override fun isEmpty(): Boolean = false
@@ -20,7 +20,7 @@ sealed class ImmutableList<T> {
 
         private tailrec fun toString(acc: String, list: ImmutableList<T>): String =
             when (list) {
-                is Nil -> acc
+                Nil -> acc
                 is Cons -> toString("$acc${list.head}, ", list.tail)
             }
     }
@@ -30,11 +30,61 @@ sealed class ImmutableList<T> {
     fun cons(item: T): ImmutableList<T> = Cons(item, this)
 
     fun setHead(item: T): ImmutableList<T> = when (this) {
-        is Nil -> throw UnsupportedOperationException("Operation not allowed")
+        Nil -> throw UnsupportedOperationException("Operation not allowed")
         is Cons -> tail.cons(item)
     }
 
+    fun drop(n: Int): ImmutableList<T> {
+        tailrec fun drop(list: ImmutableList<T>, remain: Int): ImmutableList<T> = when {
+            remain > 0 && list is Cons -> drop(list.tail, remain - 1)
+            else -> list
+        }
+        return drop(this, n)
+    }
+
+    fun dropWhile(p: (T) -> Boolean): ImmutableList<T> {
+        fun dropWhile(list: ImmutableList<T>): ImmutableList<T> = when {
+            list is Cons && p(list.head) -> dropWhile(list.tail)
+            else -> list
+        }
+        return dropWhile(this)
+    }
+
+    fun concat(list: ImmutableList<T>): ImmutableList<T> {
+        return concatRight(this, list)
+    }
+
+    fun init(): ImmutableList<T> = init(this)
+
+    fun reverse(): ImmutableList<T> = reverse(Nil as ImmutableList<T>, this)
+
     companion object {
+
+        tailrec fun <T> concatLeft(left: ImmutableList<T>, right: ImmutableList<T>): ImmutableList<T> = when (left) {
+            Nil -> right
+            is Cons -> concatLeft(left.tail, right.cons(left.head))
+        }
+
+        fun <T> concatRight(left: ImmutableList<T>, right: ImmutableList<T>): ImmutableList<T> = when (left) {
+            Nil -> right
+            is Cons -> concatRight(left.tail, right).cons(left.head)
+        }
+
+        tailrec fun <T> reverse(acc: ImmutableList<T>, list: ImmutableList<T>): ImmutableList<T> = when (list) {
+            Nil -> acc
+            is Cons -> reverse(acc.cons(list.head), list.tail)
+        }
+
+        fun <T> init(list: ImmutableList<T>): ImmutableList<T> = list.reverse().drop(1).reverse()
+
+        fun <T> initRight(list: ImmutableList<T>): ImmutableList<T> = when (list) {
+            Nil -> list
+            is Cons -> if (list.tail.isEmpty())
+                Nil as ImmutableList<T>
+            else
+                initRight(list.tail).cons(list.head)
+        }
+
         operator fun <T> invoke(vararg items: T): ImmutableList<T> =
             items.foldRight(Nil as ImmutableList<T>) { item, acc -> Cons(item, acc) }
     }
--- src/test/kotlin/pro/artkart/kmagic/list/ImmutableListTest.kt
+++ src/test/kotlin/pro/artkart/kmagic/list/ImmutableListTest.kt
@@ -3,9 +3,79 @@ package pro.artkart.kmagic.list
 import io.kotest.core.spec.style.StringSpec
 import io.kotest.matchers.shouldBe
 
-class ImmutableListTest: StringSpec({
+class ImmutableListTest : StringSpec({
 
     "ImmutableList(1, 2, 3).toString() should return [1, 2, 3, Nil]" {
-        ImmutableList(1, 2, 3).toString() shouldBe "[1, 2, 3, Nil]"
+        ImmutableList(1, 2, 3)
+            .toString() shouldBe "[1, 2, 3, Nil]"
+    }
+
+    "ImmutableList(1, 2, 3).setHead(42).toString() should return [42, 2, 3, Nil]" {
+        ImmutableList(1, 2, 3)
+            .setHead(42)
+            .toString() shouldBe "[42, 2, 3, Nil]"
+    }
+
+    "ImmutableList(1, 2, 3, 4, 5).drop(2).toString should return [3, 4, 5, Nil]" {
+        ImmutableList(1, 2, 3, 4, 5)
+            .drop(2)
+            .toString() shouldBe "[3, 4, 5, Nil]"
+    }
+
+    "ImmutableList(1, 2, 3, 4, 5).drop(10).toString should return [Nil]" {
+        ImmutableList(1, 2, 3, 4, 5)
+            .drop(10)
+            .toString() shouldBe "[Nil]"
+    }
+
+    "ImmutableList(1, 2, 3, 4, 5).drop(0).toString should return [1, 2, 3, 4, 5, Nil]" {
+        ImmutableList(1, 2, 3, 4, 5)
+            .drop(0)
+            .toString() shouldBe "[1, 2, 3, 4, 5, Nil]"
+    }
+
+    "ImmutableList(1, 2, 3, 4, 5).drop(-1).toString should return [1, 2, 3, 4, 5, Nil]" {
+        ImmutableList(1, 2, 3, 4, 5)
+            .drop(-1)
+            .toString() shouldBe "[1, 2, 3, 4, 5, Nil]"
+    }
+
+    "ImmutableList(4, 3, 2, 3).drop(2).cons(1).toString() should return [1, 2, 3, Nil]" {
+        ImmutableList(4, 3, 2, 3)
+            .drop(2)
+            .cons(1)
+            .toString() shouldBe "[1, 2, 3, Nil]"
+    }
+
+    "ImmutableList(1, 2, 3, 4).dropWhile { it < 3}.toString() should return [3, 4, Nil]" {
+        ImmutableList(1, 2, 3, 4)
+            .dropWhile { it < 3 }
+            .toString() shouldBe "[3, 4, Nil]"
+    }
+
+    "ImmutableList(1, 2, 3, 4).dropWhile { it == 42}.toString() should return [3, 4, Nil]" {
+        ImmutableList(1, 2, 3, 4)
+            .dropWhile { it == 42 }
+            .toString() shouldBe "[1, 2, 3, 4, Nil]"
+    }
+
+    "ImmutableList(1, 2, 3, 4).dropWhile { true }.toString() should return [Nil]" {
+        ImmutableList(1, 2, 3, 4)
+            .dropWhile { true }
+            .toString() shouldBe "[Nil]"
+    }
+
+    "ImmutableList(1, 2, 3).concat(ImmutableList(5, 4)).toString() should return [1, 2, 3, 5, 4, Nil]" {
+        ImmutableList(1, 2, 3)
+            .concat(ImmutableList(5, 4))
+            .toString() shouldBe "[1, 2, 3, 5, 4, Nil]"
+    }
+
+    "ImmutableList(1, 2, 3, 4).init().toString() should return [1, 2, 3, Nil]" {
+        ImmutableList(1, 2, 3, 4).init().toString() shouldBe "[1, 2, 3, Nil]"
+    }
+
+    "ImmutableList(1, 2, 3).reverse().toString() should return [3, 2, 1, Nil]" {
+        ImmutableList(1, 2, 3).reverse().toString() shouldBe "[3, 2, 1, Nil]"
     }
 })
