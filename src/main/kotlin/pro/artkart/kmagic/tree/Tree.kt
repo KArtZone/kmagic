58ad5f1b5a69dec08be1aeea36aa0aa39b2f3a02
Art Kart <akartushin@alfabank.ru>
2026-08-25 01:01:18 +0300

    Tree chapter has been supplemented

--- src/main/kotlin/pro/artkart/kmagic/tree/Tree.kt
+++ src/main/kotlin/pro/artkart/kmagic/tree/Tree.kt
@@ -1,6 +1,23 @@
 package pro.artkart.kmagic.tree
 
+import pro.artkart.kmagic.list.ImmutableList
+
 sealed class Tree<out T : Comparable<@UnsafeVariance T>> {
+
+    internal object Empty : Tree<Nothing>() {
+        override fun isEmpty(): Boolean = true
+        override fun toString(): String = "E"
+    }
+
+    internal data class Node<out T : Comparable<@UnsafeVariance T>>(
+        val left: Tree<T>,
+        val value: T,
+        val right: Tree<T>
+    ) : Tree<T>() {
+        override fun isEmpty(): Boolean = false
+
+        override fun toString(): String = "(Node $left $value $right)"
+    }
 
     abstract fun isEmpty(): Boolean
 
@@ -14,23 +31,30 @@ sealed class Tree<out T : Comparable<@UnsafeVariance T>> {
         }
     }
 
-    internal object Empty : Tree<Nothing>() {
-        override fun isEmpty(): Boolean = true
-        override fun toString(): String = "E"
-    }
-
-    internal class Node<out T : Comparable<@UnsafeVariance T>>(
-        val left: Tree<T>,
-        val value: T,
-        val right: Tree<T>
-    ) : Tree<T>() {
-        override fun isEmpty(): Boolean = false
-
-        override fun toString(): String = "(Node $left $value $right)"
+    fun contains(item: @UnsafeVariance T): Boolean = when (this) {
+        Empty -> false
+        is Node -> when {
+            item < value -> left.contains(item)
+            item > value -> right.contains(item)
+            else -> value == item
+        }
     }
 
     companion object {
 
         operator fun <T : Comparable<T>> invoke(): Tree<T> = Empty
+
+        operator fun <T : Comparable<T>> invoke(vararg items: T): Tree<T> =
+            items.fold(invoke()) { acc, item ->
+                acc + item
+            }
+
+        operator fun <T : Comparable<T>> invoke(list: ImmutableList<T>): Tree<T> =
+            list.foldLeft(invoke()) { acc -> { item -> acc + item } }
     }
 }
+
+fun <T : Comparable<T>> ImmutableList<T>.toTree(): Tree<T> =
+    foldLeft(Tree()) { acc ->
+        { item -> acc + item }
+    }
--- src/test/kotlin/pro/artkart/kmagic/tree/TreeTest.kt
+++ src/test/kotlin/pro/artkart/kmagic/tree/TreeTest.kt
@@ -2,11 +2,29 @@ package pro.artkart.kmagic.tree
 
 import io.kotest.core.spec.style.StringSpec
 import io.kotest.matchers.shouldBe
+import pro.artkart.kmagic.list.ImmutableList
 
 class TreeTest : StringSpec({
 
     "created Tree should be Empty" {
 
         Tree<Int>().isEmpty() shouldBe true
+    }
+
+    "List(2, 1, 3).toTree() should return Tree<Int>() + 2 + 1 + 3" {
+        ImmutableList(2, 1, 3)
+            .toTree() shouldBe Tree<Int>() + 2 + 1 + 3
+    }
+
+    "Tree(2, 1, 3) shouldBe Tree<Int>() + 2 + 1 + 3" {
+        Tree(2, 1, 3) shouldBe Tree<Int>() + 2 + 1 + 3
+    }
+
+    "Tree(ImmutableList(2, 1, 3)) shouldBe Tree<Int>() + 2 + 1 + 3" {
+        Tree(ImmutableList(2, 1, 3)) shouldBe Tree<Int>() + 2 + 1 + 3
+    }
+
+    "Tree(2, 1, 3).contains(2) shouldBe true" {
+        Tree(2, 1, 3).contains(2) shouldBe true
     }
 })
