2dbbc3524a8cf5806a51a4eb5d487bb047916ae6
Art Kart <akartushin@alfabank.ru>
2026-08-25 22:49:55 +0300

    Tree chapter has been supplemented

--- src/main/kotlin/pro/artkart/kmagic/tree/Tree.kt
+++ src/main/kotlin/pro/artkart/kmagic/tree/Tree.kt
@@ -1,10 +1,20 @@
 package pro.artkart.kmagic.tree
 
+import pro.artkart.kmagic.exception.Resolution
 import pro.artkart.kmagic.list.ImmutableList
+import kotlin.math.max
 
 sealed class Tree<out T : Comparable<@UnsafeVariance T>> {
 
+    abstract val size: Int
+
+    abstract val height: Int
+
+    abstract fun isEmpty(): Boolean
+
     internal object Empty : Tree<Nothing>() {
+        override val size: Int = 0
+        override val height: Int = -1
         override fun isEmpty(): Boolean = true
         override fun toString(): String = "E"
     }
@@ -14,21 +24,10 @@ sealed class Tree<out T : Comparable<@UnsafeVariance T>> {
         val value: T,
         val right: Tree<T>
     ) : Tree<T>() {
+        override val size: Int = 1 + left.size + right.size
+        override val height: Int = 1 + max(left.height, right.height)
         override fun isEmpty(): Boolean = false
-
         override fun toString(): String = "(Node $left $value $right)"
-    }
-
-    abstract fun isEmpty(): Boolean
-
-    operator fun plus(element: @UnsafeVariance T): Tree<T> = when (this) {
-        Empty -> Node(Empty, element, Empty)
-
-        is Node -> when {
-            element < value -> Node(left + element, value, right)
-            element > value -> Node(left, value, right + element)
-            else -> Node(left, element, right)
-        }
     }
 
     fun contains(item: @UnsafeVariance T): Boolean = when (this) {
@@ -37,6 +36,57 @@ sealed class Tree<out T : Comparable<@UnsafeVariance T>> {
             item < value -> left.contains(item)
             item > value -> right.contains(item)
             else -> value == item
+        }
+    }
+
+    fun max(): Resolution<T> = when (this) {
+        Empty -> Resolution.Empty
+        is Node -> right.max().orElse { Resolution(value) }
+    }
+
+    fun min(): Resolution<T> = when (this) {
+        Empty -> Resolution.Empty
+        is Node -> left.min().orElse { Resolution(value) }
+    }
+
+    fun remove(item: @UnsafeVariance T): Tree<T> = when (this) {
+        Empty -> this
+        is Node -> when {
+            item < value -> Node(left.remove(item), value, right)
+            item > value -> Node(left, value, right.remove(item))
+            else -> left + right
+        }
+    }
+
+    operator fun plus(other: Tree<@UnsafeVariance T>): Tree<T> = when (this) {
+        Empty -> other
+        is Node -> when (other) {
+            Empty -> this
+            is Node -> when {
+                value < other.value -> Node(
+                    left, value, right + Node(
+                        Empty, other.value, other.right
+                    )
+                ) + other.left
+
+                value > other.value -> Node(
+                    left + Node(
+                        other.left, other.value, Empty
+                    ), value, right
+                ) + other.right
+
+                else -> Node(left + other.left, value, right + other.right)
+            }
+        }
+    }
+
+    operator fun plus(element: @UnsafeVariance T): Tree<T> = when (this) {
+        Empty -> Node(Empty, element, Empty)
+
+        is Node -> when {
+            element < value -> Node(left + element, value, right)
+            element > value -> Node(left, value, right + element)
+            else -> Node(left, element, right)
         }
     }
 
--- src/test/kotlin/pro/artkart/kmagic/tree/TreeTest.kt
+++ src/test/kotlin/pro/artkart/kmagic/tree/TreeTest.kt
@@ -2,29 +2,59 @@ package pro.artkart.kmagic.tree
 
 import io.kotest.core.spec.style.StringSpec
 import io.kotest.matchers.shouldBe
+import pro.artkart.kmagic.exception.Resolution
 import pro.artkart.kmagic.list.ImmutableList
 
 class TreeTest : StringSpec({
+
+    val tree = Tree(2, 1, 3)
 
     "created Tree should be Empty" {
 
         Tree<Int>().isEmpty() shouldBe true
     }
 
-    "List(2, 1, 3).toTree() should return Tree<Int>() + 2 + 1 + 3" {
-        ImmutableList(2, 1, 3)
-            .toTree() shouldBe Tree<Int>() + 2 + 1 + 3
+    "tree shouldBe Tree<Int>() + 2 + 1 + 3" {
+        tree shouldBe Tree<Int>() + 2 + 1 + 3
     }
 
-    "Tree(2, 1, 3) shouldBe Tree<Int>() + 2 + 1 + 3" {
-        Tree(2, 1, 3) shouldBe Tree<Int>() + 2 + 1 + 3
+    "ImmutableList(2, 1, 3).toTree() shouldBe tree" {
+        ImmutableList(2, 1, 3).toTree() shouldBe tree
     }
 
-    "Tree(ImmutableList(2, 1, 3)) shouldBe Tree<Int>() + 2 + 1 + 3" {
-        Tree(ImmutableList(2, 1, 3)) shouldBe Tree<Int>() + 2 + 1 + 3
+    "Tree(ImmutableList(2, 1, 3)) shouldBe tree" {
+        Tree(ImmutableList(2, 1, 3)) shouldBe tree
     }
 
-    "Tree(2, 1, 3).contains(2) shouldBe true" {
-        Tree(2, 1, 3).contains(2) shouldBe true
+    "tree.contains(2) shouldBe true" {
+        tree.contains(2) shouldBe true
+    }
+
+    "tree.size shouldBe 3" {
+        tree.size shouldBe 3
+    }
+
+    "tree.height shouldBe 2" {
+        tree.height shouldBe 1
+    }
+
+    "tree.max() shouldBe Resolution.Success(3)" {
+        tree.max() shouldBe Resolution.Success(3)
+    }
+
+    "tree.min() shouldBe 0" {
+        tree.min() shouldBe Resolution.Success(1)
+    }
+
+    "Tree(2, 1, 7) + Tree(5, 3, 6) shouldBe Tree(2, 1, 7, 5, 3, 6)" {
+        Tree(2, 1, 7) + Tree(5, 3, 6) shouldBe Tree(2, 1, 7, 5, 3, 6)
+    }
+
+    "Tree(3, 1, 0, 2, 5, 4, 6).remove(5) shouldBe Tree(3, 1, 0, 2, 4, 6)" {
+        Tree(3, 1, 0, 2, 5, 4, 6).remove(5) shouldBe Tree(3, 1, 0, 2, 4, 6)
+    }
+
+    "Tree(3, 1, 0, 2, 4, 6).remove(5) shouldBe Tree(3, 1, 0, 2, 4, 6)" {
+        Tree(3, 1, 0, 2, 4, 6).remove(5) shouldBe Tree(3, 1, 0, 2, 4, 6)
     }
 })
